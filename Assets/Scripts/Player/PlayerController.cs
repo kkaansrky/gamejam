ee86d0c6c6cb4702ddc2bf5fc1c3ed1b8bfa445c
Arda Türkoğlu <ardaturkoglu2909@gmail.com>
2021-03-09 23:47:14 +0300

    Touch Controls added.
    +Devive Simulator package imported.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
     public static float _z;
     private float _speed = 5f;
     public float AmbientSpeed = 100.0f;
+    public float SwipeSpeed = 0.5f;
     private float _xRangeLeft = -2.6f;
     private float _xRangeRight = 4.6f;
     private float _tilt = 3;
@@ -33,17 +34,60 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(_xRangeRight, transform.position.y, transform.position.z);
         }
  
-        _horizontalInput = Input.GetAxis("Horizontal");
-        
+        _horizontalInput = Swipe();
+
         transform.Translate(Vector3.right * Time.deltaTime * _speed * _horizontalInput);
 
         _planeRigidbody.velocity = new Vector3(_horizontalInput, 0, 0);
         _planeRigidbody.rotation = Quaternion.Euler(0, 0, _planeRigidbody.velocity.x * -_tilt);
-        //transform.Rotate(Vector3.up, Time.deltaTime * _turnSpeed * _horizontalInput);
-    }
+        //transform.Rotate(Vector3.up, Time.deltaTime * 0.3f * _horizontalInput);
+         }
     #endregion
     #region Custom Methods
-    
+    //inside class
+    Vector2 firstPressPos;
+    Vector2 secondPressPos;
+    Vector2 currentSwipe = Vector2.zero;
+
+    public float Swipe()
+    {
+        if (Input.touches.Length > 0)
+        {
+            Touch t = Input.GetTouch(0);
+            if (t.phase == TouchPhase.Began)
+            {
+                //save began touch 2d point
+                firstPressPos = new Vector2(t.position.x, t.position.y);
+            }
+            if (t.phase == TouchPhase.Ended)
+            {
+                //save ended touch 2d point
+                secondPressPos = new Vector2(t.position.x, t.position.y);
+
+                //create vector from the two points
+                currentSwipe = new Vector3(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+                //normalize the 2d vector
+                currentSwipe.Normalize();
+
+                //swipe left
+                if (currentSwipe.x < 0 && (currentSwipe.y > -0.5f || currentSwipe.y < 0.5f))
+                {
+                    Debug.Log("left");
+                    _horizontalInput = -SwipeSpeed;
+                    return _horizontalInput;
+                }
+                //swipe right
+                if (currentSwipe.x > 0 && (currentSwipe.y > -0.5f || currentSwipe.y < 0.5f))
+                {
+                    Debug.Log("right");
+                    _horizontalInput = SwipeSpeed;
+                    return _horizontalInput;
+                }
+            }
+        }
+        return _horizontalInput;
+    }
     public static float GetZ()
     {
         return PlayerController._z;
