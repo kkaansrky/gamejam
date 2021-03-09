6f7daaf6f62c31d83df03cfd35d3da3dbb2a8a50
ugurpolat <ugurpolat113@gmail.com>
2021-03-09 22:30:51 +0300

    Bug fixes.
    Add CameraFollow script for target(plane).
    Improved PlayerController.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,18 +2,51 @@
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 5f;
-    private float turnSpeed = 20f;
-    private float horizontalInput;
-    private float forwardInput;
-    private void Update()
+    #region Variables
+    public static float _z;
+    private float _speed = 5f;
+    public float AmbientSpeed = 100.0f;
+    private float _xRangeLeft = -2.6f;
+    private float _xRangeRight = 4.6f;
+    private float _tilt = 3;
+    private float _horizontalInput;   
+    private Rigidbody _planeRigidbody;
+    #endregion
+    #region Main Methods
+    void Awake()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        _planeRigidbody = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        _z = transform.position.z;
 
+        transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+
+        if (transform.position.x < _xRangeLeft)
+        {
+            transform.position = new Vector3(_xRangeLeft, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > _xRangeRight)
+        {
+            transform.position = new Vector3(_xRangeRight, transform.position.y, transform.position.z);
+        }
+ 
+        _horizontalInput = Input.GetAxis("Horizontal");
+        
+        transform.Translate(Vector3.right * Time.deltaTime * _speed * _horizontalInput);
+
+        _planeRigidbody.velocity = new Vector3(_horizontalInput, 0, 0);
+        _planeRigidbody.rotation = Quaternion.Euler(0, 0, _planeRigidbody.velocity.x * -_tilt);
+        //transform.Rotate(Vector3.up, Time.deltaTime * _turnSpeed * _horizontalInput);
+    }
+    #endregion
+    #region Custom Methods
+    
+    public static float GetZ()
+    {
+        return PlayerController._z;
+    }
+    #endregion
 }
