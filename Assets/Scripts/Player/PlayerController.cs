9d353c5b23a4bf9890a95451eac72d7a9b856743
Arda Türkoğlu <ardaturkoglu2909@gmail.com>
2021-03-09 11:39:19 +0300

    Commit 1

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1f;
+
+    PlayerInputController inputController;
+    PlayerMovementController movementController;
+    
+
+    void Awake()
+    {
+        inputController = GetComponent<PlayerInputController>();
+        movementController = GetComponent<PlayerMovementController>();
+     
+
+        //inputController.OnSwiped += OnSwiped;  // Action<SwipeType> kullanıyor.
+        inputController.OnSwiped.AddListener(OnSwiped); // UnityEvent<SwipeType> kullanıyor. 
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    void OnSwiped(SwipeType swipeType)
+    {
+        movementController.OnSwiped(swipeType);
+
+        if (swipeType == SwipeType.Down)
+        {
+            
+        }
+    }
+}
