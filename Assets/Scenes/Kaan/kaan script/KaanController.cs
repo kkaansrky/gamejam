f9009495ccfaa411ee1ea7e549946d6551de0322
Kaan SARIKAYA <kadirkaansarikaya@gmail.com>
2021-03-11 12:32:31 +0300

    water pick

---
+++ Assets/Scenes/Kaan/kaan script/KaanController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KaanController : MonoBehaviour
+{
+    PlayerStats playerStats;
+    private void OnTriggerEnter(Collider other){
+        Debug.Log("girdi");
+        Transform tr = other.transform;
+        float y = tr.position.y-0.8f;
+        other.transform.position= new Vector3(tr.position.x,y,tr.position.z);
+        playerStats = other.GetComponent<PlayerStats>();
+
+    }
+    private void OnTriggerStay(Collider other){
+        Debug.Log("stay");
+        playerStats.addWater();
+
+    }
+    private void OnTriggerExit(Collider other){
+        Debug.Log("çıktı");
+        Transform tr = other.transform;
+        float y = tr.position.y+0.8f;
+        other.transform.position= new Vector3(tr.position.x,y,tr.position.z);
+
+    }
+    
+}
---
+++ Assets/Scenes/Kaan/kaan script/PlayerStats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    double waterSize = 0;
+
+
+    public void addWater(){
+        waterSize+=0.2;
+        Debug.Log(waterSize);
+    }
+    
+    
+}
