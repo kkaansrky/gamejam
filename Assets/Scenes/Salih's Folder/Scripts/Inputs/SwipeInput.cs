a75bcf983fa97a0221d9de9a12ed0c3e7eec7dae
Salih Gireniz <salihgireniz1@gmail.com>
2021-03-11 23:48:22 +0300

    New Player Control System

---
+++ Assets/Scenes/Salih's Folder/Scripts/Inputs/SwipeInput.cs
@@ -0,0 +1,149 @@
+﻿using UnityEngine;
+public enum SwipeState
+{
+    Default = 0,
+    Right,
+    Left,
+    Up,
+    Down
+}
+public class SwipeInput : MonoBehaviour
+{
+    // If the touch is longer than MAX_SWIPE_TIME, we dont consider it a swipe
+    public const float MAX_SWIPE_TIME = 0.5f;
+
+    // Factor of the screen width that we consider a swipe
+    // 0.17 works well for portrait mode 16:9 phone
+    public const float MIN_SWIPE_DISTANCE = 0.17f;
+
+
+    public bool debugWithArrowKeys = true;
+
+    Vector2 startPos;
+    float startTime;
+
+    public delegate void SwipeAction();
+    public static event SwipeAction swRight;
+    public static event SwipeAction swLeft;
+    public static event SwipeAction swUp;
+    public static event SwipeAction swDown;
+    public SwipeState SwipeState
+    {
+        get
+        {
+            return swipeState;
+        }
+
+        set
+        {
+            swipeState = value;
+
+            switch (swipeState)
+            {
+                case SwipeState.Default:
+                    break;
+
+                case SwipeState.Right:
+                    if (swRight != null)
+                        swRight();
+                    break;
+
+                case SwipeState.Left:
+                    if (swLeft != null)
+                        swLeft();
+                    break;
+
+                case SwipeState.Up:
+                    if (swUp != null)
+                        swUp();
+                    break;
+
+                case SwipeState.Down:
+                    if (swDown != null)
+                        swDown();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+    public SwipeState swipeState = SwipeState.Default;
+
+    public void Update()
+    {
+        if (Input.touches.Length > 0)
+        {
+            Touch t = Input.GetTouch(0);
+            if (t.phase == TouchPhase.Began)
+            {
+                startPos = new Vector2(
+                    t.position.x / (float)Screen.width, 
+                    t.position.y / (float)Screen.width
+                    );
+                startTime = Time.time;
+            }
+            if (t.phase == TouchPhase.Ended)
+            {
+                if (Time.time - startTime > MAX_SWIPE_TIME) // press too long
+                    return;
+
+                Vector2 endPos = new Vector2(
+                    t.position.x / (float)Screen.width, 
+                    t.position.y / (float)Screen.width
+                    );
+
+                Vector2 swipe = new Vector2(
+                    endPos.x - startPos.x, 
+                    endPos.y - startPos.y
+                    );
+
+                if (swipe.magnitude < MIN_SWIPE_DISTANCE) // Too short swipe
+                    return;
+
+                if (Mathf.Abs(swipe.x) > Mathf.Abs(swipe.y))
+                { // Horizontal swipe
+                    if (swipe.x > 0)
+                    {
+                        SwipeState = SwipeState.Right;
+                    }
+                    else
+                    {
+                        SwipeState = SwipeState.Left;
+                    }
+                }
+                else
+                { // Vertical swipe
+                    if (swipe.y > 0)
+                    {
+                        SwipeState = SwipeState.Up;
+                    }
+                    else
+                    {
+                        SwipeState = SwipeState.Down;
+                    }
+                }
+            }
+        }
+
+        if (debugWithArrowKeys)
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                SwipeState = SwipeState.Down;
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                SwipeState = SwipeState.Up;
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                SwipeState = SwipeState.Right;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                SwipeState = SwipeState.Left;
+            }
+        }
+    }
+}
--- Assets/Scenes/Salih's Folder/Scripts/Player/SalihPlayerController.cs
+++ Assets/Scenes/Salih's Folder/Scripts/Player/SalihPlayerController.cs
@@ -1,18 +1,116 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using DG.Tweening;
+using System.Collections;
 using UnityEngine;
-
 public class SalihPlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Event System
+    private void OnEnable()
     {
-        
+        SwipeInput.swRight += MoveRight;
+        SwipeInput.swLeft += MoveLeft;
+        SwipeInput.swUp += MoveUp;
+        SwipeInput.swDown += MoveDown;
+    }
+    private void OnDisable()
+    {
+        SwipeInput.swRight -= MoveRight;
+        SwipeInput.swLeft -= MoveLeft;
+        SwipeInput.swUp -= MoveUp;
+        SwipeInput.swDown -= MoveDown;
+    }
+    #endregion
+
+    #region Variables
+
+    [Space, SerializeField]
+    private float moveDuration;
+
+    [Space, SerializeField]
+    private float xAxisPos, minYOrdinate, maxYOrdinate;
+
+    [Space, SerializeField]
+    private float HorizontalRotation, VerticalRotation;
+
+    #endregion
+
+    #region Methods
+    public void MoveRight()
+    {
+        Debug.Log("Swiped To Right");
+
+        Vector3 newPos = new Vector3(
+            Mathf.Clamp(xAxisPos, -xAxisPos, xAxisPos), 
+            transform.position.y, 
+            transform.position.z);
+
+        Vector3 newRot = new Vector3(
+            transform.rotation.x,
+            transform.rotation.y,
+            -HorizontalRotation);
+
+        StartCoroutine(DoMove(newPos, newRot));
+    }
+    public void MoveLeft()
+    {
+        Debug.Log("Swiped To Left");
+
+        Vector3 newPos = new Vector3(
+            Mathf.Clamp(-xAxisPos, -xAxisPos, xAxisPos),
+            transform.position.y, 
+            transform.position.z);
+
+        Vector3 newRot = new Vector3(
+            transform.rotation.x,
+            transform.rotation.y,
+            HorizontalRotation);
+
+        StartCoroutine(DoMove(newPos, newRot));
+    }
+    public void MoveUp()
+    {
+        Debug.Log("Swiped To Up");
+
+        //Vector3 newPos = new Vector3(
+        //    transform.position.x,
+        //    Mathf.Clamp(maxYOrdinate, minYOrdinate, maxYOrdinate),
+        //    transform.position.z);
+
+        //Vector3 newRot = new Vector3(
+        //    -VerticalRotation,
+        //    transform.rotation.y,
+        //    transform.rotation.z);
+
+        //StartCoroutine(DoMove(newPos, newRot));
+    }
+    public void MoveDown()
+    {
+        Debug.Log("Swiped To Down");
+
+        //Vector3 newPos = new Vector3(
+        //            transform.position.x,
+        //            Mathf.Clamp(minYOrdinate, minYOrdinate, maxYOrdinate),
+        //            transform.position.z);
+
+        //Vector3 newRot = new Vector3(
+        //    VerticalRotation,
+        //    transform.rotation.y,
+        //    transform.rotation.z);
+
+        //StartCoroutine(DoMove(newPos, newRot));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator DoMove(Vector3 newPosition, Vector3 newRotation)
     {
-        
+        transform.DORotate(newRotation, .1f);
+        yield return StartCoroutine(MoveThePlayer(newPosition));
+        transform.DORotate(Vector3.zero, .1f);
     }
+
+    IEnumerator MoveThePlayer(Vector3 newPosition)
+    {
+        transform.DOMove(newPosition, moveDuration);
+        yield return new WaitForSeconds(moveDuration);
+    }
+    #endregion
+
 }
